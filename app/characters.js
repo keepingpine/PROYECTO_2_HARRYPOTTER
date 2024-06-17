d51c002680ce33979b7cd09970412e30f7c5e2fe
Gabriel <keepingpine88@gmail.com>
2024-06-17 17:27:11 -0300

    add pictures and modified index, app/characters

--- app/characters.js
+++ app/characters.js
@@ -8,38 +8,34 @@ fetch("https://hp-api.onrender.com/api/characters")
         const card = document.createElement("div");
         card.classList.add("card");
 
-        
         let imageSrc = character.image;
-
-    if (!imageSrc) {
-       
+    if (!imageSrc) {       
         if (character.alive) {
             imageSrc = "/assets/foto-perfil-generica.png";
         }else {
              imageSrc = "/assets/foto-perfil-generica-muerte.png";
         }
-
     }
-
-
         let state;
-
         if (character.alive) {
             state = "Vivo";
         }else {
             state = "Muerto";
         }
-
         card.innerHTML =  `
-                          <img src="${imageSrc}" alt="${character.name}">
-                          <div class="card-content">
-                                    <h3>${character.name}</h3>
-                                    <p><strong> Especie:</strong> ${character.species}</p>
-                                    <p><strong> Intérprete:</strong> ${character.actor}</p>
-                                    <p><strong> Estado:</strong> ${state}</p>
-                          </div>        
+                          <div class = "card-content">                              
+                            <div class = "image-content"> 
+                                <img src="${imageSrc}" alt="${character.name}">
+                            </div>
+                            <div class = "info-content">
+                                <h3>${character.name}</h3>
+                                <p><strong> Especie:</strong> ${character.species}</p>
+                                <p><strong> Intérprete:</strong> ${character.actor}</p>
+                                <p><strong> Estado:</strong> ${state}</p>
+                            </div> 
+                          <div/>       
                           `;
-                          $grid.appendChild(card);
+            $grid.appendChild(card);
         
     //}
    });
