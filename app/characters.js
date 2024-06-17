ea7088e8ed7b9edde810cb018e0da3061b5b043e
Gabriel <keepingpine88@gmail.com>
2024-06-17 15:45:57 -0300

    add fetch function on JS

--- app/characters.js
+++ app/characters.js
@@ -1,8 +1,47 @@
 fetch("https://hp-api.onrender.com/api/characters")
-.then((respuesta) => {
-    console.log(respuesta);
-    return respuesta.json();
+.then((response) => response.json())
+    .then((data) => {
+
+   const $grid = document.querySelector(".grid"); 
+   data.forEach((character) => {
+    //if (character.image) {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        
+        let imageSrc = character.image;
+
+    if (!imageSrc) {
+       
+        if (character.alive) {
+            imageSrc = "/assets/foto-perfil-generica.png";
+        }else {
+             imageSrc = "/assets/foto-perfil-generica-muerte.png";
+        }
+
+    }
+
+
+        let state;
+
+        if (character.alive) {
+            state = "Vivo";
+        }else {
+            state = "Muerto";
+        }
+
+        card.innerHTML =  `
+                          <img src="${imageSrc}" alt="${character.name}">
+                          <div class="card-content">
+                                    <h3>${character.name}</h3>
+                                    <p><strong> Especie:</strong> ${character.species}</p>
+                                    <p><strong> Intérprete:</strong> ${character.actor}</p>
+                                    <p><strong> Estado:</strong> ${state}</p>
+                          </div>        
+                          `;
+                          $grid.appendChild(card);
+        
+    //}
+   });
 })
-.then((data) => {
-    console.log(data);
-});
+
